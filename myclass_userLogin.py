7513f6a97de615a9b8533f8f8a67e28b5489b4f5
Nesta2019 <46638015+Nesta2019@users.noreply.github.com>
2019-02-27 19:46:14 -0500

    Add files via upload

---
+++ myclass_userLogin.py
@@ -0,0 +1,36 @@
+"""Hamdan alshammari
+I am going to create a class called :
+
+"userLogin.py"
+
+that will allow users to
+login into the system to use the features(user stories)
+that our project provides which are:
+1-Finding two points
+2-Finding the distance between those two points
+3-Traffic
+4-Weather
+5-Speed alert.
+Each one of these user story will be in an indivudual calss
+and we will use a main class to call them later"""
+#the name of the class will be
+class userLogin:
+    # code will be here 
+class Admin:
+        # code will be here 
+
+
+class Distance:
+        # code will be here 
+
+
+class Traffic:
+        # code will be here 
+
+
+class Weather:
+        # code will be here 
+
+
+class SpeedAlert:
+        # code will be here 
