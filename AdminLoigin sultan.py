91567996f396c2843445824dd7c04a8561485847
aljabris6699 <47014821+aljabris6699@users.noreply.github.com>
2019-02-28 01:45:11 -0500

    Add files via upload

---
+++ AdminLoigin sultan.py
@@ -0,0 +1,45 @@
+### Sultan Aljabri
+
+
+I will handle one of the user story and create a class called :
+"ِAdminLogin.py"
+this class has to contained a default user and password for the admin
+to check if it's
+the admin in the system or not.
+if he in the system he will be able to accese it
+or the system will refuse to accept his request.
+the other classes(user stories)
+that our project provides are are:
+1-User class:
+    Finding two points
+2-Admin class:
+    Finding the distance between those two points
+3-Traffic class
+4-Weather class
+
+Each one of these user story will be in an indivudual calss
+and we will use a main class to call them later"""
+
+#the name of the classes will be:
+
+class userLogin:
+    # code will be here 
+class Admin:
+        # code will be here 
+
+
+class Distance:
+        # code will be here 
+
+
+class Traffic:
+        # code will be here 
+
+
+class Weather:
+        # code will be here 
+
+
+class SpeedAlert:
+        # code will be here 
+
